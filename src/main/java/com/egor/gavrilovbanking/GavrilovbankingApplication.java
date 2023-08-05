5e095b194e104d9863c7f3d8ba459e92493dfd50
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-08-05 10:00:35 +0300

    first commit

---
+++ src/main/java/com/egor/gavrilovbanking/GavrilovbankingApplication.java
@@ -0,0 +1,13 @@
+package com.egor.gavrilovbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GavrilovbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GavrilovbankingApplication.class, args);
+	}
+
+}

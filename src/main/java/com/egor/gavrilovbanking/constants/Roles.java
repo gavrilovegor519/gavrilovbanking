046065a6da0bf09b9b12a1bf9637615c2a83e34b
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-08-25 09:09:17 +0300

    Some refactoring.

--- src/main/java/com/egor/gavrilovbanking/constants/Roles.java
+++ src/main/java/com/egor/gavrilovbanking/constants/Roles.java
@@ -1,6 +1,10 @@
 package com.egor.gavrilovbanking.constants;
 
 public class Roles {
+    private Roles() {
+
+    }
+
     public static final String ROLE_USER = "USER";
     public static final String ROLE_ADMIN = "ADMIN";
 }

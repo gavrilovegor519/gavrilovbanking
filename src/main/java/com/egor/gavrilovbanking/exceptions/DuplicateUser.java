dbc757ba2aa2a140a4a37f10eb110dc3811c1258
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-12-10 12:24:51 +0300

    Now I'm using @ResponseStatus annotation instead of using ExceptionResolver.

--- src/main/java/com/egor/gavrilovbanking/exceptions/DuplicateUser.java
+++ src/main/java/com/egor/gavrilovbanking/exceptions/DuplicateUser.java
@@ -1,5 +1,9 @@
 package com.egor.gavrilovbanking.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Duplicate user data.")
 public class DuplicateUser extends Exception {
     public DuplicateUser() {
         super();
--- src/main/java/com/egor/gavrilovbanking/exceptions/UserNotFound.java
+++ src/main/java/com/egor/gavrilovbanking/exceptions/UserNotFound.java
@@ -1,5 +1,9 @@
 package com.egor.gavrilovbanking.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "User not found.")
 public class UserNotFound extends Exception {
     public UserNotFound() {
         super();
